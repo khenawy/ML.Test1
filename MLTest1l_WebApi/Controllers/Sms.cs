af8bf18f909964492a747983bd415956622e1057
KHALED EL HENAWY <khenawy@gmail.com>
2025-01-14 11:40:04 +0000

    Adding Email

---
+++ MLTest1l_WebApi/Controllers/Sms.cs
@@ -0,0 +1,171 @@
+﻿using IPS.Notifications.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.ML;
+using MLTest1l_WebApi.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text;
+using static IPS.Notifications.DTO.Enums;
+
+namespace MLTest1l_WebApi.Controllers
+{
+    [ApiController]
+    [Route("~/sms")]
+    [AllowAnonymous]
+    public class Sms : Controller
+    {
+        private static ServiceProvider _serviceProvider = new ServiceCollection().AddHttpClient().BuildServiceProvider();
+        private static IHttpClientFactory _httpClientFactory = _serviceProvider.GetService<IHttpClientFactory>();
+        private static HttpClient _client = _httpClientFactory.CreateClient();
+        [HttpPost]
+        public async Task<EmailResults> SmsAsync([FromBody] object message)
+        {
+            System.IO.File.WriteAllText($"C:\\khaled\\{DateTime.Now.ToString("MM-dd-yyyy-hh-mm-ss")}.txt", message.ToString());
+
+            EmailResults apiResults = new EmailResults();
+            apiResults.results = new List<EmailResult>();
+
+            string json = "message is null";
+            if (message == null)
+            {
+                json += $"message_null";
+                apiResults.results.Add(new EmailResult() { toolCallId = "message_null", result = "" });
+                System.IO.File.WriteAllText($"C:\\khaled\\{DateTime.Now.ToString("MM-dd-yyyy-hh-mm-ss")}.txt", json);
+                return apiResults;
+            }
+
+            EmailRoot EmailRoot = new EmailRoot();
+            try
+            {
+                EmailRoot = JsonConvert.DeserializeObject<EmailRoot>(message.ToString());
+                json = JsonConvert.SerializeObject(message);
+            }
+            catch (Exception ex)
+            {
+                apiResults.results.Add(new EmailResult() { toolCallId = $"{ex.Message}", result = "" });
+                System.IO.File.WriteAllText($"C:\\khaled\\{DateTime.Now.ToString("MM-dd-yyyy-hh-mm-ss")}.txt", json);
+                return apiResults;
+            }
+
+            if (EmailRoot.message == null || EmailRoot.message.toolCalls == null ||
+                EmailRoot.message.toolCalls.Count <= 0 ||
+                EmailRoot.message.toolCalls[0].function.arguments is null)
+            {
+                json += $"input_null";
+                apiResults.results.Add(new EmailResult() { toolCallId = "input_null", result = "" });
+                System.IO.File.WriteAllText($"C:\\khaled\\{DateTime.Now.ToString("MM-dd-yyyy-hh-mm-ss")}.txt", json);
+                return apiResults;
+            }
+
+            string email = EmailRoot.message.toolCalls[0].function.arguments.email;
+
+            //Send email now
+            NotificationRequest objNotificationRequest = GetNotificationMessage(1, MessageType.email, 0, true, "1".ToString(), true);
+
+            //Make the http call now to post message
+            string MessageId = SendPostToNotificationService(ref _client, objNotificationRequest);
+
+            apiResults.results.Add(new EmailResult()
+            {
+                toolCallId = EmailRoot.message.toolCalls[0].id,
+                result = "email is sent successfully!"
+            });
+
+            //System.IO.File.WriteAllText($"C:\\khaled\\{DateTime.Now.ToString("MM-dd-yyyy-hh-mm-ss")}.txt", json);
+
+            //json = JsonConvert.SerializeObject(apiResults);
+            //System.IO.File.WriteAllText($"C:\\khaled\\{DateTime.Now.ToString("MM-dd-yyyy-hh-mm-ss-1")}.txt", json);
+            return apiResults;
+
+        }
+
+        private NotificationRequest GetNotificationMessage(int iCount, MessageType type, int NumSeconds, bool NeedsTemplate, string Tag, bool SingleCall)
+        {
+            int ScheduleIn = NumSeconds * 1000;
+
+            NotificationRequest objNotificationRequest = new NotificationRequest();
+            objNotificationRequest.ApplicationID = (iCount % 5) == 0 ? 1 : (iCount % 5);
+            objNotificationRequest.MessageType = MessageType.email.ToString();
+            objNotificationRequest.ScheduledIn = ScheduleIn;
+            objNotificationRequest.Subject = $"AI Agent Test";
+            objNotificationRequest.SettingId = 1;
+            if (NeedsTemplate)
+            {
+                objNotificationRequest.TemplateID = (iCount % 6) == 0 ? 1 : (iCount % 6);
+                objNotificationRequest.Body = "";
+            }
+            else
+            {
+                objNotificationRequest.TemplateID = 0;
+                objNotificationRequest.Body = "<html>Test email from an AI Agent</html>";
+            }
+            User objUser = new User();
+            objUser.SendType = SendType.to.ToString();
+            objUser.FirstName = $"Test {Tag}";
+            objUser.LastName = $"AI Agent {Tag}";
+            if (SingleCall)
+                objUser.Email = $"alaaahmed81@gmail.com";
+            else
+                objUser.Email = $"alaaahmed81@gmail.com";
+            //objUser.Phone = "+18583448390";
+            //objUser.DeviceID = "00008020-0012345A1234567B";
+            objUser.Tokens = new List<Token>();
+            objUser.Tokens.Add(new Token { Name = "FirstName", Value = objUser.FirstName });
+
+            List<User> users = new List<User>();
+            users.Add(objUser);
+            objNotificationRequest.Users = users;
+
+            //Copy these users
+            List<User> usersCopied = new List<User>();
+            usersCopied.Add(new User { SendType = SendType.cc.ToString(), Email = SendUserType.AdminEmails.ToString() });
+            usersCopied.Add(new User { SendType = SendType.bcc.ToString(), Email = SendUserType.DeveloperEmails.ToString() });
+            objNotificationRequest.UsersCopied = usersCopied;
+
+            objNotificationRequest.Tokens = new List<Token>();
+            objNotificationRequest.Tokens.Add(new Token { Name = "Message", Value = "Replacing My First token" });
+
+            return objNotificationRequest;
+        }
+        private string SendPostToNotificationService(ref HttpClient client, NotificationRequest objNotificationRequest)
+        {
+            //    System.Net.ServicePointManager.ServerCertificateValidationCallback =
+            //delegate (object s, X509Certificate certificate, X509Chain chain,
+            //SslPolicyErrors sslPolicyErrors) { return true; };
+            System.Net.ServicePointManager.ServerCertificateValidationCallback += (o, c, ch, er) => true;
+
+            //test
+            string BASIC_URL = string.Concat("https://notification.kamfr.com/api/notification");
+
+            //client.DefaultRequestHeaders.Authorization = HttpClientHelper.AddAuthentication(_settings.Username, _settings.Password);
+
+            string json = JsonConvert.SerializeObject(objNotificationRequest);
+
+
+            var content = new StringContent(json.ToString(), Encoding.UTF8, "application/json");
+            var response = client.PostAsync(BASIC_URL, content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var resp = response.Content.ReadAsStringAsync().Result;
+                NotificationResponse objNotificationResponse = JsonConvert.DeserializeObject<NotificationResponse>(resp);
+                if (objNotificationResponse.response != "Success")
+                {
+                    return objNotificationResponse.response;
+                }
+                else
+                {
+                    return objNotificationResponse.messageID.ToString();
+                }
+            }
+            else
+            {
+                return "";
+            }
+
+
+
+        }
+    }
+}
+
---
+++ MLTest1l_WebApi/Models/EmailResults.cs
@@ -0,0 +1,230 @@
+﻿using Microsoft.ML.Data;
+using static MLTest1l_WebApi.Models.Root;
+
+namespace MLTest1l_WebApi.Models
+{
+    public class EmailResult
+    {
+        [ColumnName(@"Email")]
+        public string result { get; set; }
+        public string toolCallId { get; set; }
+    }
+
+    public class EmailResults
+    {
+        public List<EmailResult> results { get; set; }
+    }
+
+    public class EmailRoot
+    {
+        public EmailMessage message { get; set; }
+
+        public class EmailArguments
+        {
+            public string email { get; set; }
+        }
+
+        public class EmailAssistant
+        {
+            public string id { get; set; }
+            public string orgId { get; set; }
+            public string name { get; set; }
+            public EmailVoice voice { get; set; }
+            public DateTime createdAt { get; set; }
+            public DateTime updatedAt { get; set; }
+            public EmailModel model { get; set; }
+            public bool recordingEnabled { get; set; }
+            public string firstMessage { get; set; }
+            public string voicemailMessage { get; set; }
+            public string endCallMessage { get; set; }
+            public EmailTranscriber transcriber { get; set; }
+            public List<string> clientMessages { get; set; }
+            public List<string> serverMessages { get; set; }
+            public List<string> endCallPhrases { get; set; }
+            public bool backchannelingEnabled { get; set; }
+            public bool backgroundDenoisingEnabled { get; set; }
+        }
+
+        public class EmailAssistantOverrides
+        {
+            public List<string> clientMessages { get; set; }
+        }
+
+        public class EmailCall
+        {
+            public string id { get; set; }
+            public string orgId { get; set; }
+            public DateTime createdAt { get; set; }
+            public DateTime updatedAt { get; set; }
+            public string type { get; set; }
+            public EmailMonitor monitor { get; set; }
+            public EmailTransport transport { get; set; }
+            public string webCallUrl { get; set; }
+            public string status { get; set; }
+            public string assistantId { get; set; }
+            public EmailAssistantOverrides assistantOverrides { get; set; }
+        }
+
+        public class EmailFunction
+        {
+            public string name { get; set; }
+            public EmailArguments arguments { get; set; }
+        }
+
+        public class EmailMessage
+        {
+            public long timestamp { get; set; }
+            public string type { get; set; }
+            public List<EmailToolCall> toolCalls { get; set; }
+            public List<EmailToolCallList> toolCallList { get; set; }
+            public List<EmailToolWithToolCallList> toolWithToolCallList { get; set; }
+            //public Artifact artifact { get; set; }
+            public EmailCall call { get; set; }
+            public EmailAssistant assistant { get; set; }
+        }
+
+        public class Message2
+        {
+            public string type { get; set; }
+            public string content { get; set; }
+            public List<object> contents { get; set; }
+            public List<object> conditions { get; set; }
+            public int? timingMilliseconds { get; set; }
+            public bool? endCallAfterSpokenEnabled { get; set; }
+            public string role { get; set; }
+            public string message { get; set; }
+            public object time { get; set; }
+            public double secondsFromStart { get; set; }
+            public double? endTime { get; set; }
+            public double? duration { get; set; }
+            public string source { get; set; }
+            public List<EmailToolCall> toolCalls { get; set; }
+        }
+
+        public class EmailMessagesOpenAIFormatted
+        {
+            public string role { get; set; }
+            public string content { get; set; }
+            public List<EmailToolCall> tool_calls { get; set; }
+            public string tool_call_id { get; set; }
+        }
+
+        public class EmailModel
+        {
+            public string model { get; set; }
+            public List<string> toolIds { get; set; }
+            public List<Message> messages { get; set; }
+            public string provider { get; set; }
+            public double temperature { get; set; }
+            public List<Tool> tools { get; set; }
+        }
+
+        public class EmailMonitor
+        {
+            public string listenUrl { get; set; }
+            public string controlUrl { get; set; }
+        }
+
+        public class EmailParameters
+        {
+            public string type { get; set; }
+            public EmailProperties properties { get; set; }
+        }
+
+        public class EmailProperties
+        {
+            public Email email { get; set; }
+            public FloorPlan floor_plan { get; set; }
+        }
+
+        public class Email
+        {
+            public string type { get; set; }
+        }
+
+        public class EmailServer
+        {
+            public string url { get; set; }
+            public int timeoutSeconds { get; set; }
+        }
+
+        public class EmailTool
+        {
+            public string id { get; set; }
+            public DateTime createdAt { get; set; }
+            public DateTime updatedAt { get; set; }
+            public string type { get; set; }
+            public EmailFunction function { get; set; }
+            public List<EmailMessage> messages { get; set; }
+            public string orgId { get; set; }
+            public Server server { get; set; }
+            public bool async { get; set; }
+        }
+
+        public class EmailToolCall
+        {
+            public string id { get; set; }
+            public string type { get; set; }
+            public EmailFunction function { get; set; }
+        }
+
+        public class EmailToolCall2
+        {
+            public string id { get; set; }
+            public string type { get; set; }
+            public EmailFunction function { get; set; }
+        }
+
+        public class EmailToolCall4
+        {
+            public string id { get; set; }
+            public string type { get; set; }
+            public EmailFunction function { get; set; }
+        }
+
+        public class EmailToolCallList
+        {
+            public string id { get; set; }
+            public string type { get; set; }
+            public EmailFunction function { get; set; }
+        }
+
+        public class EmailToolWithToolCallList
+        {
+            public string type { get; set; }
+            public EmailFunction function { get; set; }
+            public bool async { get; set; }
+            public EmailServer server { get; set; }
+            public List<EmailMessage> messages { get; set; }
+            public EmailToolCall toolCall { get; set; }
+        }
+
+        public class EmailTranscriber
+        {
+            public string model { get; set; }
+            public string language { get; set; }
+            public string provider { get; set; }
+        }
+
+        public class EmailTransport
+        {
+            public bool assistantVideoEnabled { get; set; }
+        }
+
+        public class FloorPlan
+        {
+            public string type { get; set; }
+        }
+
+        public class EmailVoice
+        {
+            public string model { get; set; }
+            public string voiceId { get; set; }
+            public string provider { get; set; }
+            public bool fillerInjectionEnabled { get; set; }
+        }
+    }
+
+
+
+}
